e9cd44619dd65c8d5e3400deef11ef71c5976a17
John Viscel <john_viscel@yahoo.com>
2013-06-21 15:07:26 +0800

    Exer1 version 2

--- SangkalWX6LExercise1/src/Dream.java
+++ SangkalWX6LExercise1/src/Dream.java
@@ -3,7 +3,7 @@ public class Dream {
 	
 	public static void main(String[] args){
 		
-		System.out.println("I will pass CMSC 22!");
+		System.out.println("I will pass CMSC 22 and all my subjects this semester!");
 		
 	}
 }

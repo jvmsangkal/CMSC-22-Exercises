337a01b24767164be80c38980ba7414b1fcbc0f6
John Viscel <john_viscel@yahoo.com>
2013-07-19 15:41:48 +0800

    sample for reference

---
+++ Laboratory5/src/LaboratorySample5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class LaboratorySample5 {
+/*
+	public static void main(String args[]){
+		System.out.println("Enter the pet owner's name:");
+		Scanner sc= new Scanner(System.in);
+		String name=sc.nextLine();
+		//po=new PetOwner();
+		//po.name=name;
+		PetOwner po=new PetOwner(name);
+		PetOwner.numGlobalPetOwners++;
+		System.out.println("Hello "+po.name);
+		System.out.println("Enter the pet owner's name:");
+		name =sc.nextLine();
+		PetOwner po2 =new PetOwner(name);
+		PetOwner.numGlobalPetOwners++;
+		System.out.println("Hello "+po2.name);
+		//System.out.println("Number of pet Owners is "+PetOwner.numGlobalPetOwners);
+	}*/
+}

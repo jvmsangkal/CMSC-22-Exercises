6c403e0472cfc0810a609e0936e6390fcdc05321
John Viscel Sangkal <johnviscelsangkal@gmail.com>
2013-09-14 01:29:39 +0800

    gumagana na,panget layout at may weird bug

--- Exercise10/src/Paint.java
+++ Exercise10/src/Paint.java
@@ -4,66 +4,129 @@ import java.awt.event.MouseEvent;
 import javax.swing.*;
 
 public class Paint {
-	
-	
+	static PaintJPanel pane1 = new PaintJPanel();
+	static JButton shape1= new JButton();
+	static JButton shape2= new JButton();
+	static JButton shape3= new JButton();
+	static JButton shape4= new JButton();
+	static JButton color1= new JButton();
+	static JButton color2= new JButton();
+	static JButton color3= new JButton();
+	static JButton color4= new JButton();
+	static JButton color5= new JButton();
 	public static void main(String[] args) {
 		JFrame mf = new JFrame("Paint");
 		mf.setSize(700,700);
+		Icon circle=new ImageIcon("circle.gif");
+		Icon triangle=new ImageIcon("triangle.gif");
+		Icon cross=new ImageIcon("cross.jpeg");
+		Icon hexagon=new ImageIcon("hexagon.png");
+		//shape1.setIcon(circle);
+		//shape2.setIcon(triangle);
+		//shape3.setIcon(cross);
+		//shape4.setIcon(hexagon);
 		
+		shape1.setName("circle");
+		shape2.setName("triangle");
+		shape3.setName("cross");
+		shape4.setName("hexagon");
 		
-		PaintJPanel pane1 = new PaintJPanel();
 		pane1.setBackground(Color.WHITE);
+		color1.setBackground(Color.RED);
+		color2.setBackground(Color.GREEN);
+		color3.setBackground(Color.BLUE);
+		color4.setBackground(Color.YELLOW);
+		color5.setBackground(Color.BLACK);
 		
 		JPanel pane = new JPanel();
 		JPanel colors = new JPanel();
 		JPanel brushes = new JPanel();
+		
 		mf.setContentPane(pane);
 		pane.setLayout(new BorderLayout());
-		pane.add(pane1,BorderLayout.CENTER);
 		pane.add(colors,BorderLayout.WEST);
+		pane.add(pane1,BorderLayout.CENTER);
 		pane.add(brushes,BorderLayout.EAST);
 		
 		brushes.setLayout(new GridLayout(4,1));
-		JButton[] brush = new JButton[4];
-		for(int i=0;i<4;i++){
-			brush[i]=new JButton();
-			brushes.add(brush[i]);
-			brush[i].addMouseListener(new MouseAdapter() {
-				public void mouseClicked(MouseEvent e){
-					
-				}
-			});
-		}
-		brush[0].setText("circle");
-		brush[1].setText("triangle");
-		brush[2].setText("cross");
-		brush[3].setText("hexagon");
+		brushes.add(shape1);
+		brushes.add(shape2);
+		brushes.add(shape3);
+		brushes.add(shape4);
 		
-		JButton[] color = new JButton[5];
-		colors.setLayout(new GridLayout(5,1));
-		for(int i=0;i<5;i++){
-			color[i]=new JButton();
-			colors.add(color[i]);
-			color[i].addMouseListener(new MouseAdapter() {
-				public void mouseClicked(MouseEvent e){
-					
-				}
-			});
-		}
-		color[0].setBackground(Color.RED);
-		color[1].setBackground(Color.BLUE);
-		color[2].setBackground(Color.GREEN);
-		color[3].setBackground(Color.YELLOW);
-		color[4].setBackground(Color.BLACK);
-		
-		pane1.addMouseListener(new MouseAdapter() {
-		
-			public void mouseDragged(MouseEvent e){
-				PaintJPanel.x=e.getX();
-				PaintJPanel.y=e.getY();
+		shape1.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e){
+				pane1.setShape(shape1.getName());
 			}
 		});
-		pane1.repaint();
+		shape2.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e){
+				pane1.setShape(shape2.getName());
+			}
+		});
+		shape3.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e){
+				pane1.setShape(shape3.getName());
+			}
+		});
+		shape4.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e){
+				pane1.setShape(shape4.getName());
+			}
+		});
+		
+		colors.setLayout(new GridLayout(5,1));
+		colors.add(color1);
+		colors.add(color2);
+		colors.add(color3);
+		colors.add(color4);
+		colors.add(color5);
+		
+		color1.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e){
+				pane1.setC(color1.getBackground());
+			}
+		});
+		color2.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e){
+				pane1.setC(color2.getBackground());
+			}
+		});
+		color3.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e){
+				pane1.setC(color3.getBackground());
+			}
+		});
+		color4.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e){
+				pane1.setC(color4.getBackground());
+			}
+		});
+		color5.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e){
+				pane1.setC(color5.getBackground());
+			}
+		});
+		pane1.addMouseMotionListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e){
+				pane1.setX(e.getX());
+				pane1.setY(e.getY());
+				pane1.setClicked(true);	
+				//pane1.paintComponent(pane1.getGraphics());
+				
+			}
+			public void mouseDragged(MouseEvent e){
+				pane1.setX(e.getX());
+				pane1.setY(e.getY());
+				pane1.setClicked(true);
+				pane1.repaint();
+				//pane1.paintComponent(pane1.getGraphics());
+			}
+			public void mouseReleased(MouseEvent e){
+				pane1.setClicked(false);	
+			}
+		});
+		
 		mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mf.setVisible(true);
 	}
--- Exercise10/src/PaintJPanel.java
+++ Exercise10/src/PaintJPanel.java
@@ -1,38 +1,94 @@
 import javax.swing.*;
-
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 
 public class PaintJPanel extends JPanel{
-	public static String Shape="circle";
-	public static Color c;
-	public static int x;
-	public static int y;
-	
+	private String Shape="circle";
+	private Color c=Color.BLACK;
+	private int x;
+	private int y;
+	private boolean clicked;
 	
 	public PaintJPanel() {
-		
+		super();
+		this.setBackground(Color.WHITE);
+	}
+
+	public PaintJPanel(String shape, Color c, int x, int y, boolean clicked) {
+		super();
+		Shape = shape;
+		this.c = c;
+		this.x = x;
+		this.y = y;
+		this.clicked = clicked;
+	}
+
+	public String getShape() {
+		return Shape;
+	}
+
+	public void setShape(String shape) {
+		Shape = shape;
+	}
+
+	public Color getC() {
+		return c;
+	}
+
+	public void setC(Color c) {
+		this.c = c;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public boolean isClicked() {
+		return clicked;
+	}
+
+	public void setClicked(boolean clicked) {
+		this.clicked = clicked;
 	}
 
 	public void paintComponent(Graphics g){
 		super.paintComponents(g);
 		g.setColor(c);
+		if (clicked==true){
 		if(Shape.equals("circle")){
-			g.fillArc(x, y, 17, 17, 0, 360);
-			
+			g.fillArc(x-9, y-9, 18, 18, 0, 360);	
 		}else if(Shape.equals("triangle")){
-		
-			
+			int[] xPoints={x-20,x,x+20};
+			int[] yPoints={y+10,y-10,y+10};
+			g.fillPolygon(xPoints, yPoints, 3);
+			int[] xPoints2={x,x-10,x+10};
+			int[] yPoints2={y+10,y,y};
+			g.setColor(Color.WHITE);
+			g.fillPolygon(xPoints2, yPoints2, 3);
+			g.setColor(c);
 		}else if(Shape.equals("cross")){
-			
-			
+			g.fillRect(x-10, y-20, 15, 40);
+			g.fillRect(x-20, y-10, 35, 15);
 		}else if(Shape.equals("hexagon")){
-			
-			
+			int[] xpoints={x-15,x,x+15,x+15,x,x-15};
+			int[] ypoints={y+10,y+20,y+10,y-10,y-20,y-10};
+			g.fillPolygon(xpoints,ypoints,6);
 		}
-		repaint();
+		}
 		
 	}
 }
